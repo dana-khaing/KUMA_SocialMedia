0c720c861576626062493bdcfc8e1c94fa18fc73
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-10-29 01:47:57 +0000

    delete README.md in frontend

--- product/frontend/src/components/stories.js
+++ product/frontend/src/components/stories.js
@@ -1,10 +1,15 @@
+import Image from "next/image";
 const Stories = () => {
   return (
     <div className="w-full h-[10.5rem]  rounded-2xl items-center shadow-md overflow-scroll text-xs bg-slate-50 scrollbar-hide">
       <div className="flex gap-8 items-center m-4 w-max">
         <div className="flex flex-col items-center gap-1 cursor-pointer justify-start h-full w-[5rem]">
-          <img
+          {/* use this first one to use the image component from next js */}
+          <Image
             src="/stories1.jpg"
+            alt="stories"
+            width={45}
+            height={45}
             className="w-[4.5rem] h-[4.5rem] rounded-full ring-2 hover:ring-4 ring-[#FF4E01] m-1"
           />
           <span className="text-[#FF4E01]">Dana</span>
