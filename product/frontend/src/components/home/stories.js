cf48db1dca46d47a9d6f1bbf74022ed25fd0a16b
Dana Khaing <dana1352000.uk@gmail.com>
2024-10-31 18:47:28 +0000

    refactor the friendrequest component for responsive and add raw data to test

--- product/frontend/src/components/home/stories.js
+++ product/frontend/src/components/home/stories.js
@@ -4,9 +4,9 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "../ui/button";
 const Stories = () => {
   return (
-    <div className="w-full h-fit flex-shrink-0 rounded-3xl items-center shadow-md overflow-scroll text-xs border-[1px] bg-slate-50 scrollbar-hide">
+    <div className="w-full h-fit flex-shrink-0 rounded-3xl overflow-scroll items-center p-4 shadow-md text-xs border-[1px] bg-slate-50 scrollbar-hide">
       {/* stories warp */}
-      <div className="flex gap-1 md:gap-5 items-center m-4 w-max ">
+      <div className="flex gap-1 md:gap-5 items-center w-max ">
         {/* Add stories for the user */}
         <div className="flex flex-col items-center gap-1 cursor-pointer justify-start h-full w-[5rem]">
           <div className="w-[4.5rem] h-[4.5rem] rounded-full ring-2 hover:ring-4 ring-[#FF4E01] m-1 flex items-center justify-center">
