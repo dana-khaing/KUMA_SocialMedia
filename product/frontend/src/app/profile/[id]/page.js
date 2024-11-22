1cb2fd1a902fe3b2c873d947628823a9d20e9bc2
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-11-22 18:31:02 +0000

    add user profile card in profile page

--- product/frontend/src/app/profile/[id]/page.js
+++ product/frontend/src/app/profile/[id]/page.js
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
 
+import FriendRequest from "@/components/home/friendRequest";
+import OnlineFriends from "@/components/home/onlineFriends";
+import ProfileBigCard from "@/components/home/profileBigCard";
+import UsefulTool from "@/components/home/usefulTool";
+import { useUser } from "@clerk/nextjs";
 const Profilepage = () => {
-  return <div>Profilepage</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-[120vh] w-screen flex items-start justify-center gap-4 py-4">
+      {/* left */}
+      <div className="hidden lg:block flex-col w-[25%]">
+        <FriendRequest />
+        <UsefulTool />
+      </div>
+      {/* center */}
+      <div className="flex w-full flex-col lg:w-[50%] gap-5">
+        <div className="flex flex-col gap-5 w-[100%] h-[150vh] overflow-y-scroll scrollbar-hide overscroll-x-none">
+          <ProfileBigCard user={user} />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="hidden lg:flex flex-col gap-5 w-[25%]">
+        <OnlineFriends />
+      </div>
+    </div>
+  );
 };
 
 export default Profilepage;
---
+++ product/frontend/src/components/home/profileBigCard.js
@@ -0,0 +1,69 @@
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+import { useClerk } from "@clerk/nextjs";
+export const ProfileBigCard = ({ user }) => {
+  const { openUserProfile } = useClerk();
+
+  const handleEditProfile = () => {
+    openUserProfile(); // Opens the Clerk "Manage Account" modal
+  };
+  return (
+    <div className="w-full bg-slate-50 rounded-2xl shadow-md text-sm border-[1px] flex-col cursor-default overflow-hidden pb-4 mb-5">
+      <div className="h-[15rem]">
+        {/* Cover photo */}
+        <div className="relative w-full mb-12 h-[75%]">
+          <img
+            src="/stories1.jpg"
+            alt="Coverphoto"
+            className="rounded-t-2xl fill-transparent w-full h-full object-cover"
+          />
+          <img
+            src={user?.imageUrl || "user-default.png"}
+            alt="profile"
+            className="w-28 h-28 rounded-full ring-4 ring-white -bottom-14 absolute left-1/2 transform -translate-x-1/2 bg-white object-cover"
+          />
+        </div>
+      </div>
+      {/* Full name and username */}
+      <div className="flex flex-col items-center justify-center px-4">
+        <span className="text-black text-2xl font-bold">
+          {user?.fullName || "Kuma User"}
+        </span>
+        <span className="text-gray-500">@{user?.username || "username"}</span>
+      </div>
+      {/* Bio */}
+      <div className="flex flex-col items-center justify-center px-4">
+        <span className="text-black font-bold">Bio</span>
+        <span className="text-gray-500 line-clamp-1">
+          {user?.bio || "No bio yet, Kuma"}
+        </span>
+      </div>
+      {/* User's friends, followers, and following */}
+      <div className="flex justify-center items-center  text-base gap-2 mt-4">
+        <div className="flex flex-col text-center items-center justify-center">
+          <span className="text-black font-bold">Friends</span>
+          <span className="text-gray-500">0</span>
+        </div>
+        <Separator orientation="vertical" className="h-8 bg-[#FF4E01]" />
+        <div className="flex flex-col text-center items-center justify-center">
+          <span className="text-black font-bold">Followers</span>
+          <span className="text-gray-500">0</span>
+        </div>
+        <Separator orientation="vertical" className="h-8 bg-[#FF4E01]" />
+        <div className="flex flex-col text-center items-center justify-center">
+          <span className="text-black font-bold">Following</span>
+          <span className="text-gray-500">0</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <Button
+          onClick={handleEditProfile}
+          className="w-36 rounded-full bg-[#FF4E01] text-white hover:text-[#FF4E01] hover:drop-shadow-lg hover:bg-white h-fit cursor-pointer gap-2 text-center justify-center"
+        >
+          <span>Edit Profile</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default ProfileBigCard;
