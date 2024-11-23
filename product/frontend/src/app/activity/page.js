315d4c89fb84b7866175c988ff1b238df3b5a015
Dana Khaing <dana1352000.uk@gmail.com>
2024-11-23 02:37:11 +0000

    add friendlist , notification, group and studio components and connect  to all button

---
+++ product/frontend/src/app/activity/page.js
@@ -0,0 +1,34 @@
+"use client";
+import FriendRequest from "@/components/home/friendRequest";
+import Birthday from "@/components/home/birthday";
+import OnlineFriends from "@/components/home/onlineFriends";
+import ProfileSmallCard from "@/components/home/profileSmallCard";
+import UsefulTool from "@/components/home/usefulTool";
+import { useUser } from "@clerk/nextjs";
+
+export const Activity = () => {
+  const { user } = useUser();
+  return (
+    <div className="h-[120vh] w-screen flex items-start justify-center gap-4 py-4">
+      {/* left */}
+      <div className="hidden lg:block flex-col w-[25%]">
+        <ProfileSmallCard user={user} />
+        <UsefulTool />
+      </div>
+      {/* center */}
+      <div className="flex w-full flex-col lg:w-[50%] gap-5">
+        <div className="flex flex-col gap-5 w-[100%] h-[150vh] overflow-y-scroll scrollbar-hide overscroll-x-none">
+          {/*  */}
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="hidden lg:flex flex-col gap-5 w-[25%]">
+        <FriendRequest />
+        <Birthday />
+        <OnlineFriends />
+      </div>
+    </div>
+  );
+};
+export default Activity;
---
+++ product/frontend/src/app/friendlist/page.js
@@ -0,0 +1,34 @@
+"use client";
+import FriendRequest from "@/components/home/friendRequest";
+import Birthday from "@/components/home/birthday";
+import OnlineFriends from "@/components/home/onlineFriends";
+import ProfileSmallCard from "@/components/home/profileSmallCard";
+import UsefulTool from "@/components/home/usefulTool";
+import { useUser } from "@clerk/nextjs";
+import Newfeed from "@/components/home/newfeed";
+
+export const Friendlist = () => {
+  const { user } = useUser();
+  return (
+    <div className="h-[120vh] w-screen flex items-start justify-center gap-4 py-4">
+      {/* left */}
+      <div className="hidden lg:block flex-col w-[25%]">
+        <ProfileSmallCard user={user} />
+        <UsefulTool />
+      </div>
+      {/* center */}
+      <div className="flex w-full flex-col lg:w-[50%] gap-5">
+        <div className="flex flex-col gap-5 w-[100%] h-[150vh] overflow-y-scroll scrollbar-hide overscroll-x-none">
+          <Birthday />
+          <FriendRequest />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="hidden lg:flex flex-col gap-5 w-[25%]">
+        <OnlineFriends />
+      </div>
+    </div>
+  );
+};
+export default Friendlist;
