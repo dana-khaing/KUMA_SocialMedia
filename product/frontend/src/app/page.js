607ac3a50a1deb507447d5c30961bc07c098f306
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-11-24 02:23:08 +0000

    implement clientprisma, run test for data fetching , test successful ,eject client side clerk data fetching  and removed dead code

--- product/frontend/src/app/page.js
+++ product/frontend/src/app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import Stories from "@/components/home/stories";
 import Addpost from "@/components/home/addpost";
 import Newfeed from "@/components/home/newfeed";
@@ -7,35 +6,21 @@ import Birthday from "@/components/userfriends/birthday";
 import OnlineFriends from "@/components/userfriends/onlineFriends";
 import ProfileSmallCard from "@/components/userInfo/profileSmallCard";
 import UsefulTool from "@/components/home/usefulTool";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useUser, useSignOut } from "@clerk/nextjs";
+
 export default function Home() {
-  const { isLoaded, isSignedIn, user } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isLoaded && (!isSignedIn || !user)) {
-      router.push("/sign-in");
-    }
-  }, [isLoaded, isSignedIn, user, router]);
-
-  if (!isLoaded || !isSignedIn || !user) {
-    return null; // Avoid rendering content before redirect
-  }
   return (
     <div className="h-[120vh] w-screen flex items-start justify-center gap-4 py-4">
       {/* left */}
       <div className="hidden lg:block flex-col w-[25%]">
-        <ProfileSmallCard user={user} />
+        <ProfileSmallCard />
         <UsefulTool />
       </div>
       {/* center */}
       <div className="flex w-full flex-col lg:w-[50%] gap-5">
         <div className="flex flex-col gap-5 w-[100%] h-[150vh] overflow-y-scroll scrollbar-hide overscroll-x-none">
           <Stories />
-          <Addpost user={user} />
-          <Newfeed user={user} />
+          <Addpost />
+          <Newfeed />
         </div>
       </div>
 
