1d336743f8773d554b53a203b3d425921aca5135
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-12-04 22:23:37 +0000

    separate and create setting component to fix error of client side rendering inside sever-only component.

---
+++ product/frontend/src/components/home/settingAction.js
@@ -0,0 +1,25 @@
+"use client";
+import { useClerk } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+
+export const SettingAction = () => {
+  const { openUserProfile } = useClerk();
+  const handleEditProfile = () => {
+    openUserProfile();
+  };
+  return (
+    <div>
+      <Button
+        onClick={handleEditProfile}
+        className=" flex-grow  h-fit items-center bg-transparent text-base shadow-md w-full text-black hover:bg-[#FF4E02] hover:text-white py-4 px-8 rounded-xl justify-start"
+      >
+        <FontAwesomeIcon icon={faCog} size="md" className="mr-2" />
+        Setting
+      </Button>
+    </div>
+  );
+};
+export default SettingAction;
