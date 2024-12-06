dd734ea8901471a81a20e9e60640a38d573ab4ff
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-12-06 06:32:53 +0000

    sycn user following into all friends component

--- product/frontend/src/components/userfriends/checkfriends.js
+++ product/frontend/src/components/userfriends/checkfriends.js
@@ -1,6 +1,19 @@
 import { Separator } from "@radix-ui/react-dropdown-menu";
-import Dummyfriends from "./dummyfriends";
-export const Checkfriends = () => {
+import { CheckfriendsAction } from "./checkfriendsAction";
+import { auth } from "@clerk/nextjs/server";
+export const Checkfriends = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    return null;
+  }
+  const friendlist = await prisma.follower.findMany({
+    where: {
+      followerId: userId,
+    },
+    include: {
+      following: true,
+    },
+  });
   return (
     <div className=" w-full h-[55rem] bg-slate-50 rounded-2xl shadow-md text-sm border-[1px] flex-shrink-0 flex-col pt-4 cursor-default ">
       <div className="flex items-center justify-between px-4">
@@ -13,41 +26,7 @@ export const Checkfriends = () => {
       {/* Friend List */}
       <div className="h-[50rem] overflow-y-scroll scrollbar-hide flex flex-col gap-1 px-4">
         {/* Friend Card */}
-        <div className="flex gap-2 items-center hover:bg-slate-200 p-2 rounded-xl">
-          <div className="flex-shrink-0  rounded-full bg-white items-center justify-center mr-2">
-            <img
-              src="/stories1.jpg"
-              alt="profile"
-              className="w-12 h-12 rounded-full ring-1 ring-[#FF4E01]"
-            />
-          </div>
-          <div className="flex flex-col flex-grow cursor-pointer">
-            <span className="text-black text-sm line-clamp-1">
-              Hein Htet Aung
-            </span>
-            <span className="text-gray-500 text-xs line-clamp-1">
-              2 mutual friends
-            </span>
-          </div>
-        </div>
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
-        <Dummyfriends />
+        <CheckfriendsAction friendlist={friendlist} />
       </div>
     </div>
   );
