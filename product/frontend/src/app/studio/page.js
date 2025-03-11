156ecd9d92ce00328bc7f93a9631fa3ffcf611d3
Dana Khaing <dana1352000.uk@gmail.com>
2025-03-11 19:37:24 +0000

    refactor search component to be able to use in mobile and adjust all of the component to become user friendly in mobile version

--- product/frontend/src/app/studio/page.js
+++ product/frontend/src/app/studio/page.js
@@ -7,7 +7,7 @@ import Newfeed from "@/components/home/newfeed";
 
 export const Studio = () => {
   return (
-    <div className="h-[120vh] w-full flex items-start justify-center gap-4 p-4">
+    <div className="h-screen w-full flex items-start justify-center gap-4 p-4">
       {/* left */}
       <div className="hidden lg:flex grow-0 flex-col gap-5 w-[25%]">
         <ProfileSmallCard />
@@ -15,7 +15,7 @@ export const Studio = () => {
       </div>
       {/* center */}
       <div className="flex w-screen px-2 shrink-0 flex-col lg:w-[50%] gap-5">
-        <div className="flex flex-col gap-5 w-[100%] h-[150vh] overflow-y-scroll scrollbar-hide overscroll-x-none">
+        <div className="flex flex-col gap-5 w-[100%] h-screen overflow-y-scroll scrollbar-hide overscroll-x-none">
           <Newfeed />
         </div>
       </div>
--- product/frontend/src/components/home/searchUser.js
+++ product/frontend/src/components/home/searchUser.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Separator } from "@radix-ui/react-dropdown-menu";
+
 export const SearchUser = () => {
   const [open, setOpen] = useState(false);
   const handleClose = () => {
@@ -11,18 +12,19 @@ export const SearchUser = () => {
 
   return (
     <div>
-      <div
-        onClick={() => setOpen(true)}
-        className="w-full flex-grow  h-fit items-center bg-transparent shadow-md text-base text-black hover:bg-[#FF4E02] hover:text-white py-4 px-8 rounded-xl justify-start"
-      >
-        <FontAwesomeIcon icon={faSearch} size="md" className="mr-2" />
-        Search
+      <div onClick={() => setOpen(true)}>
+        <FontAwesomeIcon
+          icon={faSearch}
+          size="md"
+          className="lg:mr-2 text-[#FF4E02] "
+        />
+        <span className="hidden lg:inline">Search</span>
       </div>
       {open && (
-        <div className=" absolute w-screen h-[162vh] bg-black top-0 backdrop:blur-md left-0 bg-opacity-35 flex items-center justify-center z-50">
-          <form className="p-5 bg-white rounded-lg shadow-md flex flex-col bottom-[32rem] gap-4 w-[70%] md:w-[35%] xl:w-[30%] relative">
-            <div className="flex justify-around text-base text-[#FF4E01] items-center h-6">
-              <span className="flex flex-1 px-5 ">Search</span>
+        <div className="fixed w-screen h-screen bg-black top-0 bg-opacity-50 left-0 flex items-center justify-center z-50">
+          <form className="p-5 bg-white rounded-lg shadow-md flex flex-col gap-4 w-[90%] sm:w-[80%] md:w-[60%] lg:w-[45%] xl:w-[30%] relative">
+            <div className="flex justify-between text-base text-[#FF4E01] items-center h-6">
+              <span className="flex flex-1 px-5">Search</span>
               <span
                 className="w-8 h-8 hover:bg-gray-200 rounded-full flex items-center justify-center cursor-pointer"
                 onClick={handleClose}
@@ -31,10 +33,10 @@ export const SearchUser = () => {
               </span>
             </div>
             {/* search box */}
-            <div className="w-full justify-center items-center px-5 ">
+            <div className="w-full justify-center items-center px-5">
               <input
                 type="text"
-                placeholder=" Wanna find someone , Kuma?"
+                placeholder="Wanna find someone, Kuma?"
                 className="w-full border-2 border-gray-300 rounded-lg p-2"
               />
             </div>
