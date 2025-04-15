054fea75e036bbabb9f0ec46d75abf0cdca7b1c7
Dana Khaing <WLIS410@live.rhul.ac.uk>
2025-04-15 16:33:36 +0100

    show the current path by nav item

--- product/frontend/src/components/navbar/navitem.js
+++ product/frontend/src/components/navbar/navitem.js
@@ -1,20 +1,39 @@
+"use client";
+
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faRss } from "@fortawesome/free-solid-svg-icons";
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
-import { auth } from "@clerk/nextjs/server";
+import {
+  faHouse,
+  faBell,
+  faUser,
+  faRss,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { useUser } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 
-export default async function NavItem() {
-  const { userId } = await auth();
+export default function NavItem() {
+  const { user } = useUser();
+  const userId = user?.id;
+  const pathname = usePathname();
+
+  // Define routes
+  const routes = {
+    home: "/",
+    friendlist: "/friendlist",
+    studio: "/studio",
+    profile: userId ? `/profile/${userId}` : "/profile",
+    activity: "/activity",
+  };
+
   return (
     <div>
-      <div className=" items-center gap-2 hidden md:flex justify-around">
+      <div className="items-center gap-2 hidden md:flex justify-around">
         <Link
-          href={"/"}
-          className="flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl"
+          href="/"
+          className={`flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl ${
+            pathname === routes.home ? "bg-[#FF4E02] text-white" : ""
+          }`}
         >
           <FontAwesomeIcon
             alt="home"
@@ -24,8 +43,10 @@ export default async function NavItem() {
           />
         </Link>
         <Link
-          href={"/friendlist"}
-          className="flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl"
+          href="/friendlist"
+          className={`flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl ${
+            pathname === routes.friendlist ? "bg-[#FF4E02] text-white" : ""
+          }`}
         >
           <FontAwesomeIcon
             className="w-[1.75rem] h-[1.75rem]"
@@ -34,8 +55,10 @@ export default async function NavItem() {
           />
         </Link>
         <Link
-          href={"/studio"}
-          className="flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl"
+          href="/studio"
+          className={`flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl ${
+            pathname === routes.studio ? "bg-[#FF4E02] text-white" : ""
+          }`}
         >
           <FontAwesomeIcon
             className="w-[1.75rem] h-[1.75rem] text-sm"
@@ -43,17 +66,19 @@ export default async function NavItem() {
             size="lg"
           />
         </Link>
-
         <Link
           href={`/profile/${userId}`}
-          className="flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl"
+          className={`flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl ${
+            pathname === routes.profile ? "bg-[#FF4E02] text-white" : ""
+          }`}
         >
           <FontAwesomeIcon className="w-6 h-6" icon={faUser} size="lg" />
         </Link>
-
         <Link
-          href={"/activity"}
-          className="flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl"
+          href="/activity"
+          className={`flex w-28 h-12 items-center shadow justify-center mt-4 mb-4 text-[#FF4E01] hover:bg-[#FF4E01] hover:text-white rounded-3xl ${
+            pathname === routes.activity ? "bg-[#FF4E02] text-white" : ""
+          }`}
         >
           <FontAwesomeIcon className="w-6 h-6" icon={faBell} size="lg" />
         </Link>
