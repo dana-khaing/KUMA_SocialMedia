c3820e1bc011e4b064de979c5b681ab064812d97
Dana Khaing <dana1352000.uk@gmail.com>
2026-06-03 19:57:13 +0100

    Apply consistent fixed-column scroll layout to activity, friendlist, post, and profile pages

--- product/frontend/src/app/friendlist/page.js
+++ product/frontend/src/app/friendlist/page.js
@@ -5,25 +5,23 @@ import Checkfriends from "@/components/userfriends/checkfriends";
 
 export const Friendlist = () => {
   return (
-    <div className="h-screen w-full flex items-start justify-center gap-4 p-4">
+    <div className="flex min-h-[calc(100vh-5rem)] w-full items-start justify-center gap-4 p-4 lg:h-[calc(100vh-5rem)] lg:overflow-hidden lg:px-4">
       {/* left */}
-      <div className="hidden lg:flex grow-0 flex-col gap-5 w-[25%]">
-        <ProfileSmallCard />
-        <UsefulTool />
+      <div className="hidden min-h-0 grow-0 flex-col gap-5 overflow-y-auto scrollbar-hide lg:flex lg:w-[25%] lg:h-full">
+        <div className="shrink-0"><ProfileSmallCard /></div>
+        <div className="shrink-0"><UsefulTool /></div>
       </div>
       {/* center */}
-      <div className="flex w-screen px-2 shrink-0 flex-col lg:w-[50%] gap-5">
-        <div className="flex flex-col gap-5 w-[100%] h-screen overflow-y-scroll scrollbar-hide overscroll-x-none">
-          <FriendRequest />
-          <Checkfriends />
-        </div>
-      </div>
-
-      {/* right */}
-      <div className="hidden lg:flex grow-0 flex-col gap-5 w-[25%]">
+      <div className="flex w-screen min-h-0 shrink-0 flex-col gap-5 px-2 lg:h-full lg:w-[50%] lg:overflow-y-auto lg:scrollbar-hide">
+        <FriendRequest />
         <Checkfriends />
+      </div>
+      {/* right */}
+      <div className="hidden min-h-0 grow-0 flex-col gap-5 overflow-y-auto scrollbar-hide lg:flex lg:w-[25%] lg:h-full">
+        <div className="shrink-0"><Checkfriends /></div>
       </div>
     </div>
   );
 };
+
 export default Friendlist;
