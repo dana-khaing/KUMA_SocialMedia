d32d734748a484b17dc3fc6f18159e95814d36ba
Dana Khaing <WLIS410@live.rhul.ac.uk>
2026-06-03 19:53:55 +0100

    Fix sidebar cards compressing by wrapping in shrink-0 divs

--- product/frontend/src/app/page.js
+++ product/frontend/src/app/page.js
@@ -152,8 +152,8 @@ export default async function Home() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] w-full items-start justify-center gap-4 p-4 lg:h-[calc(100vh-5rem)] lg:overflow-hidden lg:px-4">
       <div className="hidden min-h-0 grow-0 flex-col gap-5 overflow-y-auto scrollbar-hide lg:flex lg:w-[25%] lg:h-full">
-        <ProfileSmallCard />
-        <UsefulTool />
+        <div className="shrink-0"><ProfileSmallCard /></div>
+        <div className="shrink-0"><UsefulTool /></div>
       </div>
 
       <div className="flex w-screen min-h-0 shrink-0 flex-col px-2 lg:h-full lg:w-[50%] lg:overflow-y-auto lg:scrollbar-hide">
@@ -164,8 +164,8 @@ export default async function Home() {
       </div>
 
       <div className="hidden min-h-0 grow-0 flex-col gap-5 overflow-y-auto scrollbar-hide lg:flex lg:w-[25%] lg:h-full">
-        <FriendRequest />
-        <Checkfriends />
+        <div className="shrink-0"><FriendRequest /></div>
+        <div className="shrink-0"><Checkfriends /></div>
       </div>
     </div>
   );
